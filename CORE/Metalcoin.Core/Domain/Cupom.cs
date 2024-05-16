98562c048f315efdf74ce127ae2c6f6069a30398
Senai <Senai@CEPEAS21103945>
2024-05-15 21:25:50 -0300

    Feat: Initial comit das propriedades da nova atribuição cupom;

---
+++ CORE/Metalcoin.Core/Domain/Cupom.cs
@@ -0,0 +1,23 @@
+﻿using Metalcoin.Core.Abstracts;
+using Metalcoin.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metalcoin.Core.Domain
+{
+    public class Cupom : Entidade
+    {
+        public string CodigoCupom { get; set; }
+        public string Descricao { get; set; }
+        public decimal ValorDesconto { get; set; }
+        public int QtdCuponsLiberados { get; set; }
+        public int QtdCuponsUsados { get; set; }
+        public TipoDesconto TipoDesconto { get; set; }
+        public StatusCupom Status { get; set; }
+        public DateTime DataValidade { get; set; }
+
+    }
+}
---
+++ CORE/Metalcoin.Core/Dtos/Request/CupomCadastrarRequest.cs
@@ -0,0 +1,28 @@
+﻿using Metalcoin.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Metalcoin.Core.Dtos.Request
+{
+    public class CupomCadastrarRequest
+    {
+        [Required(ErrorMessage = "Codigo do Cupom é obrigatorio é obrigátorio")]
+        public string CodigoCupom { get; set; }
+
+
+        [MaxLength(100, ErrorMessage = "Descrição pode ter no máximo 100 caracteres")]
+        public string Descricao { get; set; }
+
+        public decimal ValorDesconto { get; set; }
+        public TipoDesconto TipoDesconto { get; set; }
+      
+        public int QtdCuponsLiberados { get; set; }
+     
+        public StatusCupom Status { get; set; }
+        
+    }
+}
